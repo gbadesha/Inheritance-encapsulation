223c5365bc0ca00fc53f48915d217d1415df9ac1
gbadesha <gurpreetmba@gmail.com>
2023-03-06 16:31:06 -0500

    first commit

--- src/inheritancePackage/Child.java
+++ src/inheritancePackage/Child.java
@@ -4,7 +4,8 @@ public class Child extends Parent{
     public void printA(){
         System.out.println(a);
     }
-    public void display(){
+    public void display(){   //method overriding
         System.out.println("this is a display method of child");
+        super.display();// will access display method of parent class
     }
 }
--- src/inheritancePackage/GrandChild.java
+++ src/inheritancePackage/GrandChild.java
@@ -3,6 +3,7 @@ package inheritancePackage;
 public class GrandChild extends Child {
     int e;
     public void show(){
+        super.display();
         System.out.println(" entered value of e in grandchild class " +e +" from child a "+a);
     }
 
